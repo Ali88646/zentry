80754781e136450abb196014bfcd3b5d5e86e89c
Your Name <you@example.com>
2025-01-17 19:23:49 +0500

    added story section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,20 @@
-const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
+import { twMerge } from "tailwind-merge";
+
+const Button = ({
+  title,
+  id,
+  rightIcon,
+  leftIcon,
+  containerClass,
+  className = "",
+}) => {
   return (
     <div
       id={id}
-      className={`group relative z-10 w-fit cursor-pointer overflow-hidden animate-button px-7 py-2 text-black rounded-full ${containerClass} `}
+      className={twMerge(
+        `group relative z-10 w-fit cursor-pointer overflow-hidden animate-button px-7 py-2 text-black rounded-full ${containerClass}`,
+        className
+      )}
     >
       {leftIcon}
       <span className="relative  inline-flex overflow-hidden font-general text-xs uppercase">
